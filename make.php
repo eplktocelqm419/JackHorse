574b98ddd8c30f9fd402089dde2fefb042489d7a
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-04-14 00:16:12 +0800

    1

--- make.php
+++ make.php
@@ -1,10 +1,10 @@
 <?php
 
-for($i=10000;$i<15000;$i++){
+for ($i = 0; $i < 150000; $i++) {
     $rand = rand(1, 15);
     $content = [
-        "name"=>"JackHorse #".$i,
-        "image"=>"https://eplktocelqm419.github.io/JackHorse/images/$rand.png",
+        "name" => "JackHorse #" . $i,
+        "image" => "https://eplktocelqm419.github.io/JackHorse/images/$rand.png",
     ];
-    file_put_contents("./docs/json/$i.json", json_encode($content));
+    file_put_contents("./docs/json/$i.json", json_encode($content,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
 }
